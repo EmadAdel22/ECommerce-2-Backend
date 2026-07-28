aa50b9549eef37bccb2ba3f3a332482208e9e0a0
EmadAdel22 <emadadelgabar@gmail.com>
2026-07-28 12:14:16 +0300

    Add infrasutcture registration And ICategory Interface

---
+++ Ecom/Ecom.infrastructure/Repositires/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ecom.core.Entities.Products;
+using Ecom.core.Interfaces;
+using Ecom.infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Ecom.infrastructure.Repositires
+{
+    public class CategoryRepository : GeniricRepositire<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
