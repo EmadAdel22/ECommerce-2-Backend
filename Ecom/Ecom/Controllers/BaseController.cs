a318f60d6f2620c7abfab116570c4ba79cfde87e
EmadAdel22 <emadadelgabar@gmail.com>
2026-07-28 16:11:14 +0300

    Add Get all cateegories , get by it end points

---
+++ Ecom/Ecom/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected readonly IUnitOfWork work;
+
+        public BaseController(IUnitOfWork work)
+        {
+            this.work = work;
+        }
+    }
+}
--- Ecom/Ecom/Controllers/CategoriesController.cs
+++ Ecom/Ecom/Controllers/CategoriesController.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Ecom.core.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecom.Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoriesController : ControllerBase
+  
+    public class CategoriesController : BaseController
     {
-    }
+        public CategoriesController(IUnitOfWork work) : base(work)
+        {
+
+        }
+
+        [HttpGet("get-all")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            try
+            {
+                var categories = await work.categoryRepository.GetAllAsync();
+                if (categories == null)
+                    return BadRequest();
+                return Ok(categories);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+               
+            }
+        }
+
+        [HttpGet("get-by-id/{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            try
+            {
+                var category = await work.categoryRepository.GetByIdAsync(id);
+                if (category == null)
+                    return BadRequest();
+                return Ok(category);
+
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+
+
+        }
 }
