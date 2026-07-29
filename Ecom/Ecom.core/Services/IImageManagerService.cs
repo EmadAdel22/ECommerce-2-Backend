8b04df02a032ca0dc5912aa240f11c9f3f548d10
EmadAdel22 <emadadelgabar@gmail.com>
2026-07-29 14:23:57 +0300

    add service to manage images

---
+++ Ecom/Ecom.core/Services/IImageManagerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Ecom.core.Services
+{
+    public interface IImageManagerService
+    {
+        Task<List<string>> AddImageAysnc(IFormFileCollection files, string src);
+
+        Task DeletImageAsync(string src);
+
+    }
+}
---
+++ Ecom/Ecom.infrastructure/Repositires/Services/ImageManagerService.cs
@@ -0,0 +1,27 @@
+﻿using Ecom.core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecom.infrastructure.Repositires.Services
+{
+    public class ImageManagerService : IImageManagerService
+    {
+         private readonly IFileProvider _fileProvider;
+        public ImageManagerService(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+        public Task<List<string>> AddImageAysnc(IFormFileCollection files, string src)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletImageAsync(string src)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
