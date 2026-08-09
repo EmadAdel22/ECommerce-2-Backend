f2b31de7b3d1261befbf24c237986b4c4a44c580
EmadAdel22 <emadadelgabar@gmail.com>
2026-08-09 15:19:03 +0300

    Add basketItem and CustomerBasket entities

---
+++ Ecom/Ecom.core/Entities/BasketItems.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecom.core.Entities
+{
+    public class BasketItems
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
---
+++ Ecom/Ecom.core/Entities/CustmoerBasket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecom.core.Entities
+{
+    public class CustmoerBasket
+    {
+         
+        public CustmoerBasket()
+        {
+            
+        }
+
+        public CustmoerBasket(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+        public List<BasketItems> Items { get; set; } = new List<BasketItems>();
+    }
+}
