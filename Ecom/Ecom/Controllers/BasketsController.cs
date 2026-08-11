14a5fadcd1d1440c648624b847123c3cf77cf3e8
EmadAdel22 <emadadelgabar@gmail.com>
2026-08-11 17:41:30 +0300

    Add Customer Basket Controll implementation

---
+++ Ecom/Ecom/Controllers/BasketsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Ecom.core.Entities;
+using Ecom.core.Interfaces;
+using Ecom.infrastructure.Repositires;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecom.Api.Controllers
+{
+
+    public class BasketsController : BaseController
+    {
+        public BasketsController(IUnitOfWork work, IMapper mapper) : base(work, mapper)
+        {
+        }
+
+        [HttpGet("get-basket-item/{id}")]
+
+        public async Task<IActionResult> getBasketItems(string id)
+        {
+            var result = await work.customerBasketReposatory.GetBasketAsync(id);
+            return Ok(result);
+        }
+
+        [HttpPost("update-basket")]
+
+        public async Task<IActionResult> AddItem(CustmoerBasket basket)   
+        {
+            var _basket = await work.customerBasketReposatory.updateBasketAsync(basket);
+            return Ok(basket);
+
+        }
+
+        [HttpDelete("delet-basket-item/{id}")]
+        public async Task<IActionResult> DeletItem(string id)
+        {
+           var result = await work.customerBasketReposatory.DeletBasketAsync(id);
+            return result ? Ok("Item Ddeleted") : BadRequest();
+        }
+
+    }
+}
